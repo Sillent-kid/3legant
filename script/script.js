a120b1a947a7a12178001ef647842e96aa5f5b1d
promise ushiagwu <ushiagwup@gmail.com>
2024-12-13 12:16:31 +0100

    add programmes files

---
+++ script/script.js
@@ -0,0 +1,75 @@
+// let box = 100;
+// let firstName = "promise";
+// let year = 2024;
+// let lastName = "Ushiagwu";
+// year = 2025;
+// let fullname = firstName + lastName;
+// console.log(fullname);
+// let x = 16 + "volvo"
+// console.log(x)
+// typeof(x)
+// console.log(typeof x)
+// let y = "lexus" + 350;
+// alert(y);
+// //strings
+// let message = 'he said,"jesus wept"';
+// let fullName1 = `${firstName} ${lastName}`;
+// console.log(fullName1);
+// let name = "promise ushiagwu";
+// let facility = "Tech creek";
+// let course = "web dev";
+// let study = name + " " + "studies" + " " + course + " " + "at" + " " + facility;
+// console.log(study);
+// let sent = `${name} studies ${course} at ${facility}`
+// alert(sent)
+// // Object
+// const myUser = {
+//     firstName,
+//     lastName,
+//     yob: 2000,
+//     eyeColor: "brown",
+//     course,
+//     facility,
+//     printFullname: function() {
+//         return this.firstName + " " +this.lastName;
+//     },
+// };
+// myUser.friends = [
+//     "wisdom",
+//     "promie",
+//     "peter",
+//     "martha",
+//     "pys"
+// ] 
+// console.log(myUser.friends[3])
+// // let age = 2024 - myUser.yob
+// // alert(age)
+
+
+// // Object AAND PROPERTIES
+
+// myUser.stateoforigin = ("delta")
+// console.log(myUser)
+// myUser["local government"] = "obio_apkor";
+// console.log(myUser)
+// myUser.printAge = function() {
+//     return 2024 - myUser.yob
+// }
+// myUser.printFullname ()
+// console.log(myUser.printFullname ())
+// // ARRAY
+// const number = [
+//     1,2,3,4,5,6,7,8,9,10,
+// ]
+// const names  = [
+//     mattew,
+//     john,
+//     peter,
+//     paul,
+//     satan,
+// ]
+// if  (h == 11 && m == 20){
+//     console.log("the time is 11:20")
+// }if (1 && 0 ){
+//     alert("is it falsy OR truthy")
+// }
